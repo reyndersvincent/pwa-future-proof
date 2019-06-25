79570f1719a805e3ddb4929fc88daba5ae6862b5
Joey Comhaire <joey.comhaire@gmail.com>
2019-06-25 11:24:04 +0200

    sw

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import register from './register';
 
-const swURL = './serviceworker.js';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
-register(swURL);
+register();
 
