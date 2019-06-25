527a477aac262a8a7c1465b1c32d27f7b8407426
Joey Comhaire <joey.comhaire@gmail.com>
2019-06-25 17:01:23 +0200

    fixed functions

--- functionsnetlify/pokeapi.js
+++ functionsnetlify/pokeapi.js
@@ -5,12 +5,17 @@ const FIXED_PART = '.netlify/functions/pokeapi/'
 
 exports.handler = async function(event, context, callback) {
   const URL = `${API_ENDPOINT}${event.path.replace(FIXED_PART, '')}`.replace('/api/api', '/api');
-  console.log(URL);
   return fetch(URL, { headers: { 'Accept': 'application/json'} })
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
+      headers: {
+        'Content-type': 'application/json'
+      },
       body: data
     }))
-    .catch(error => ({ statusCode: 422, body: String(error)}));
+    .catch(error => {
+      console.log(error);
+      return { statusCode: 422, body: String(error)};
+    });
 }
