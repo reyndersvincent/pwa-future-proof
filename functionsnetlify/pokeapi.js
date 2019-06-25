1e7cb0964a431610f9f8c55b7d8c4814c35990f3
Joey Comhaire <joey.comhaire@gmail.com>
2019-06-25 15:24:30 +0200

    added node-fetch

--- functionsnetlify/pokeapi.js
+++ functionsnetlify/pokeapi.js
@@ -3,7 +3,8 @@ import fetch from "node-fetch";
 const API_ENDPOINT = "https://pokeapi.co/api/v2";
 
 exports.handler = async function(event, context, callback) {
-  return fetch(API_ENDPOINT, { headers: { 'Accept': 'application/json'} })
+  console.log(event);
+  return fetch(`${API_ENDPOINT}/${event.path}`, { headers: { 'Accept': 'application/json'} })
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
