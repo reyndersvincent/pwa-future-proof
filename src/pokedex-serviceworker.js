98f22b37a9db890636254e4f3b9252c19d17a085
VincentReynders <vincent.reynders@student.kdg.be>
2019-06-26 12:17:38 +0200

    added register install and activate

---
+++ src/pokedex-serviceworker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(event) {
+    console.log('Installed');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Activated');
+});
--- src/register.ts
+++ src/register.ts
@@ -1,5 +1,13 @@
 const register = (swUrl = '', config = {}) => {
-
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+          navigator.serviceWorker.register('/pokedex-serviceworker.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }, function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+          });
+        });
+      }
 };
 
 export default register;
